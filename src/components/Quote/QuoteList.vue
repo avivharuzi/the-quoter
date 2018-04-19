ea1022c98dfbf1f50500070e7326ae2a9b13be3f
avivharuzi <avivharuzi@gmail.com>
2018-04-19 23:10:42 +0300

    Added quote list code

---
+++ src/components/Quote/QuoteList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">
+    <app-quote-item v-for="quote in quotes" :key="quote" :quote="quote"></app-quote-item>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      quotes: Array
+    }
+  }
+</script>
